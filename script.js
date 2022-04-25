4d6d29bc8d2a079852eb1664ddc3f5faf416c005
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-04-24 22:17:49 -0400

    Changed fetch API endpoint to improve search generalization at the expense of some location accuracy.

--- script.js
+++ script.js
@@ -1,91 +1,99 @@
 // assign DOM elements to JavaScript variables
 var userFormEl = document.querySelector("#form");
 var userInputEl = document.querySelector("#input-location");
+var currentCityDivEl = document.querySelector("#current-city-div");
+var cityHeaderEl = document.querySelector("#city-header");
+var cityListEl = document.querySelector("#city-list");
 
 // store openweather API key in variable
 const myWeatherApiKey = "&appid=db5c0fd2b45b6f4bb5fa58e25f4eec1d";
 
 // check the user has at least entered a comma (",") in the input field
-var checkSyntaxAndReturnArray = function(location){
+var checkSyntaxAndReturnArray = function (location) {
     // console.log('Inside checkSyntaxAndReturnArray');
     var hasComma = location.includes(',');
-    
-    if(hasComma){
-        let array = location.split(',').map( string => string.trim());
+
+    if (hasComma) {
+        let array = location.split(',').map(string => string.trim());
         console.log(array);
         return array;
-    }else{
+    } else {
         return false;
     }
 }
 
-async function getLatAndLong(locationArray){
+async function getLocationWeatherObject(locationArray) {
     // console.log('Inside getLatAndLong');
-    const geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct";
-    const limit = "&limit=5";
-    const locationString = locationArray.join();
-    const apiUrl = geocodeUrl + "?q=" + locationString + limit + myWeatherApiKey;
-
-    try{
+    const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+    const limit = "&limit=3";
+    const locationString = locationArray.join(',');
+    // console.log('locationString is', locationString);
+    const apiUrl = weatherUrl + "?q=" + locationString + limit + myWeatherApiKey;
+    try {
         let response = await fetch(apiUrl);
         let data = await response.json();
+        console.log(data);
+        if (data.cod === "404") {
+            return;
+        }
         return data;
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
 
 // get weather information
-async function getAllWeather(locationObj){
-    const lon = "&lon=" + locationObj.lon;
-    const lat = "lat=" + locationObj.lat;
+async function getCurrentAndForecastWeather(locationObj) {
+    const lon = "&lon=" + locationObj.coord.lon;
+    const lat = "lat=" + locationObj.coord.lat;
     const baseUrl = "https://api.openweathermap.org/data/2.5/onecall";
     const myUnits = "&units=imperial";
-    const exclude = "&exclude=minutely,hourly,";
+    const exclude = "&exclude=minutely,hourly,alerts";
     const apiUrl = baseUrl + "?" + lat + lon + exclude + myWeatherApiKey + myUnits;
-    
-    try{
+
+    try {
         let response = await fetch(apiUrl);
         let data = await response.json();
         return data;
-    }catch(error){
+    } catch (error) {
         console.log(error);
+
     }
 };
 
-function displayWeather(locationObj){
+function displayWeather(locationObj) {
 
 }
 
 // function to handle user submission
-async function formSubmitHandler(event){
+async function formSubmitHandler(event) {
     // prevent default behavior of browser to refresh web page upon user submission
     event.preventDefault();
     // remove whitespace from both ends of string
     const location = userInputEl.value.trim();
-    var weatherData = {};
+    var locationObject = {};
     // check to make sure user entered some string
-    if(location){
+    if(!location) {
+        alert("Empty string detected. Please enter a location.");
+    }else{
         // reset input field value to empty string
         userInputEl.value = "";
         // check for syntax and return array
-        var locationArray = checkSyntaxAndReturnArray(location);
-        
-        if(locationArray.length){
-            const [ returnedLocation ] = await getLatAndLong(locationArray);
-            weatherData = await getAllWeather(returnedLocation);
-            console.log('The weather is', weatherData);
-
-            // displayWeather(weatherData);
-        }else{
+        var inputSplitArray = checkSyntaxAndReturnArray(location);
+        if(!inputSplitArray) {
             alert("It appears you did not enter the information as required."
-            +"\nEnter the city,state(,country) separating them with a comma."
-            +"\nFor example: Des Moines,Iowa,US or Des Moines, IA, US");
+                + "\nEnter the city,state(,country) separating them with a comma."
+                + "\nFor example: Des Moines,Iowa,USA");
+        }else{
+            locationObject = await getLocationWeatherObject(inputSplitArray);
+            console.log('locationObject is', locationObject);
+
+            locationObject ?
+            weatherData = await getCurrentAndForecastWeather(locationObject) :
+            alert("It appears there was a problem in the process of carrying out the search."
+            + "\nThis is likely an Error 404: resource not found. Please try your search again.");
+            // displayWeather(weatherData)
         }
-        
-        
-    } else{
-        alert("Empty string detected. Please enter a location.");
     }
 }
 
